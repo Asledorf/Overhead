3e700d6af43b9ea84e4979fc268efa2fcfa356b9
Kenneth Stover <KStover@student.neumont.edu>
2022-03-10 04:40:19 -0700

    invisible char

--- Assets/scripts/AudioManager.cs
+++ Assets/scripts/AudioManager.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-﻿using UnityEngine.Audio;
+using UnityEngine.Audio;
 using UnityEngine;
 using System;
 using UnityEngine.SceneManagement;
